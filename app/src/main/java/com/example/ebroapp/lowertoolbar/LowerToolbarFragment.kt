def9df08e29c9e88dff2a48c56ad646799e6ae29
Sergey Lebedev <sergey.lebedev@novator-it.ru>
2022-02-23 18:45:04 +0300

    add view binding into LowerToolbarFragment

--- app/src/main/java/com/example/ebroapp/lowertoolbar/LowerToolbarFragment.kt
+++ app/src/main/java/com/example/ebroapp/lowertoolbar/LowerToolbarFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.ebroapp.R
+import com.example.ebroapp.base.BaseFragment
+import com.example.ebroapp.databinding.FragmentLowerToolbarPlaceholderBinding
+import com.example.ebroapp.databinding.FragmentUserInfoBinding
 
-class LowerToolbarFragment : Fragment() {
+class LowerToolbarFragment : BaseFragment<FragmentLowerToolbarPlaceholderBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,4 +22,7 @@ class LowerToolbarFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_lower_toolbar_placeholder, container, false)
     }
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentLowerToolbarPlaceholderBinding
+            = FragmentLowerToolbarPlaceholderBinding::inflate
 }
