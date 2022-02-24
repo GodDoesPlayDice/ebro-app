3f93120a6c9f8b439cfe680c594918727a230d9f
slowfarm <Altair08111994@yandex.ru>
2022-02-25 02:54:33 +0300

    add bluetooth receiver

--- app/src/main/java/com/example/ebroapp/receiver/BluetoothConnectionReceiver.kt
+++ app/src/main/java/com/example/ebroapp/receiver/BluetoothConnectionReceiver.kt
@@ -1,5 +1,6 @@
 package com.example.ebroapp.receiver
 
+import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -8,15 +9,15 @@ import android.content.Intent.ACTION_POWER_DISCONNECTED
 import com.example.ebroapp.view.activity.black.BlackActivity
 import com.example.ebroapp.view.activity.MainActivity
 
-class ActionPowerReceiver : BroadcastReceiver() {
+class BluetoothConnectionReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         when (intent?.action) {
-            ACTION_POWER_CONNECTED -> {
+            BluetoothDevice.ACTION_ACL_CONNECTED -> {
                 context.startActivity(Intent(context, MainActivity::class.java).apply {
                     addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 })
             }
-            ACTION_POWER_DISCONNECTED -> {
+            BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
                 context.startActivity(Intent(context, BlackActivity::class.java).apply {
                     addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 })
