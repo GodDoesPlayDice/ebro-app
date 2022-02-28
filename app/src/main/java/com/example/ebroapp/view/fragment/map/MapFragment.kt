9b93be330d66cc301547db148b6c6d5a851b4f32
slowfarm <Altair08111994@yandex.ru>
2022-02-28 12:04:30 +0300

    add map

--- app/src/main/java/com/example/ebroapp/view/fragment/map/MapFragment.kt
+++ app/src/main/java/com/example/ebroapp/view/fragment/map/MapFragment.kt
@@ -1,18 +1,78 @@
 package com.example.ebroapp.view.fragment.map
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.example.ebroapp.databinding.FragmentMapBinding
 import com.example.ebroapp.view.base.BaseFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.LatLng
+
 
 class MapFragment : BaseFragment<FragmentMapBinding>() {
+
+    private var map: GoogleMap? = null
 
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentMapBinding =
         FragmentMapBinding::inflate
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.mapView.onCreate(savedInstanceState);
+
+        binding.mapView.onResume()
+
+        try {
+            MapsInitializer.initialize(requireContext())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        binding.mapView.getMapAsync {
+            map = it.apply {
+                uiSettings.isMyLocationButtonEnabled = false
+                if (ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        ACCESS_FINE_LOCATION
+                    ) != PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        ACCESS_COARSE_LOCATION
+                    ) != PERMISSION_GRANTED
+                ) {
+                    isMyLocationEnabled = true
+                    moveCamera(CameraUpdateFactory.newLatLng(LatLng(43.1, -87.9)))
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        binding.mapView.onResume()
+        super.onResume()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onDestroyView() {
+        binding.mapView.onDestroy()
+        super.onDestroyView()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
--- app/src/main/java/com/example/ebroapp/view/fragment/mapfull/MapFullFragment.kt
+++ app/src/main/java/com/example/ebroapp/view/fragment/mapfull/MapFullFragment.kt
@@ -1,16 +1,24 @@
 package com.example.ebroapp.view.fragment.mapfull
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.example.ebroapp.App
-import com.example.ebroapp.R
 import com.example.ebroapp.databinding.FragmentMapFullBinding
 import com.example.ebroapp.view.base.BaseFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.LatLng
 
 class MapFullFragment : BaseFragment<FragmentMapFullBinding>() {
+
+
+    private var map: GoogleMap? = null
 
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentMapFullBinding =
         FragmentMapFullBinding::inflate
@@ -22,5 +30,55 @@ class MapFullFragment : BaseFragment<FragmentMapFullBinding>() {
         binding.btnPlay.setOnCheckedChangeListener { _, isChecked ->
             App.get().playerDelegate.playPauseMusic(isChecked)
         }
+
+        binding.mapView.onCreate(savedInstanceState);
+
+        binding.mapView.onResume()
+
+        try {
+            MapsInitializer.initialize(requireContext())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        binding.mapView.getMapAsync { googleMap ->
+            map = googleMap
+            map?.apply {
+                uiSettings.isMyLocationButtonEnabled = false
+                if (ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    isMyLocationEnabled = true
+                }
+                moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(41.385063, 2.173404), 1f))
+            }
+        }
+    }
+
+    override fun onResume() {
+        binding.mapView.onResume()
+        super.onResume()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onDestroyView() {
+        binding.mapView.onDestroy()
+        super.onDestroyView()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
