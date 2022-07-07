83cea3f1453887f89c0e2b2a026a5185cddccb2b
slowfarm <Altair08111994@yandex.ru>
2022-07-07 11:02:48 +0300

    version upgrade

--- app/src/main/java/com/example/ebroapp/view/fragment/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/ebroapp/view/fragment/weather/WeatherViewModel.kt
@@ -3,12 +3,12 @@ package com.example.ebroapp.view.fragment.weather
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.ebroapp.domain.repository.DomainRepository
 import com.example.ebroapp.remote.entity.weather.FullWeather
 import com.example.ebroapp.remote.repository.RemoteRepository
 import com.example.ebroapp.utils.launchIO
 import com.example.ebroapp.utils.withMain
-import kotlinx.coroutines.GlobalScope
 import timber.log.Timber
 
 class WeatherViewModel(application: Application) : AndroidViewModel(application) {
@@ -19,7 +19,7 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
     private val domainRepository = DomainRepository.obtain()
 
     fun getCurrentLocation() {
-        GlobalScope.launchIO({
+        viewModelScope.launchIO({
             domainRepository.getCurrentLocation()?.let { point ->
                 val response = remoteRepository.getWeatherFull(point.latitude(), point.longitude())
                 withMain { weather.value = response }
